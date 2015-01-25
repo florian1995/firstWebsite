fc62b061f8e0680190496902e8613b3945756b7f
Florian Platte <florian-platte@web.de>
2015-01-25 21:43:10 +0100

    - test

---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+	$('#animation').dblclick(function() {
+        $('#animation').fadeOut('fast'); 
+    });
+	
+	$('#explode').dblclick(function() {
+        $('#explode').effect("explode",1000); 
+    });
+    
+	
+	
+});
